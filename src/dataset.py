8e1cfa5c05835baf1d5672b5bcc5e3e777f4e903
Kaushik Acharya <acharya.kaushik@gmail.com>
2020-07-21 16:18:13 +0530

    Introduced Line, Sentence as separate classes. Line represents protocol step whereas Sentence represents linguistic sentence segmentation.

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,13 @@
 
 """
 Load dataset and the annotations.
+
+Data Format:
+-----------
+http://noisy-text.github.io/2020/wlp-task.html#format
+
+Each line of the protocol text indicates a single step in the protocol.
+Observation: Step can have multiple sentences e.g. protocol_101 (line #2)
 """
 
 
@@ -10,14 +17,76 @@ import os
 import re
 
 from .annotation import *
+from .nlp_process import *
+
+
+class Line:
+    """Protocol text line representing a step in protocol.
+        A step consists of single or multiple sentences.
+    """
+    def __init__(self, start_char_pos=None, end_char_pos=None, start_word_index=None, end_word_index=None,
+                 start_sent_index=None, end_sent_index=None):
+        self.start_char_pos = start_char_pos
+        self.end_char_pos = end_char_pos
+        self.start_word_index = start_word_index
+        self.end_word_index = end_word_index
+        self.start_sent_index = start_sent_index
+        self.end_sent_index = end_sent_index
+
+
+class Word:
+    """Word class is associated with Line class."""
+    def __init__(self, start_char_pos=None, end_char_pos=None):
+        self.start_char_pos = start_char_pos
+        self.end_char_pos = end_char_pos
+
+
+class Sentence:
+    def __init__(self, start_char_pos=None, end_char_pos=None, start_token_index=None, end_token_index=None):
+        self.start_char_pos = start_char_pos
+        self.end_char_pos = end_char_pos
+        self.start_token_index = start_token_index
+        self.end_token_index = end_token_index
+
+
+class Token:
+    """Token class is associated with Sentence class.
+        Though this is similar to Word but created using different tokenizer(spaCy)
+            which leads to difference in few words.
+            Word represents token created by tokenizer which was used to named entities of train data.
+
+        Additionally Token's tokenizer also extracts
+            a) part of speech tagging
+            b) syntactic parsing
+    """
+    def __init__(self, start_char_pos=None, end_char_pos=None, part_of_speech=None, dependency_tag=None, head_index=None, children_index_arr=None):
+        self.start_char_pos = start_char_pos
+        self.end_char_pos = end_char_pos
+        self.part_of_speech = part_of_speech
+        self.dependency_tag = dependency_tag
+        self.head_index = head_index
+        self.children_index_arr = children_index_arr
+
 
 class Document:
-    def __init__(self, doc_id):
+    """Protocol document"""
+    def __init__(self, doc_id, nlp_process_obj):
         self.id = doc_id
         self.text = None
+
+        # Line represents a protocol step. This may contain multiple sentences.
+        self.lines = []
+        self.words = []  # usually represents a space separated token in text.
+
+        # Sentence represents a sentence in linguistic context.
         self.sentences = []
-        self.words = []
+        # Token represents a token formed by NLP engine.
+        self.tokens = []
+        # Example: 37°C - This word is represented by 3 tokens in spaCy: 37, °, C
+
         self.entity_annotations = []
+
+        self.nlp_process_obj = nlp_process_obj
 
     def parse_document(self, document_file):
         """Load the document text
@@ -34,48 +103,52 @@ class Document:
     def parse_conll_annotation(self, conll_ann_file, verbose=False):
         assert self.text is not None, "parse_document() is a pre-requisite"
         with codecs.open(filename=conll_ann_file, mode="r", encoding="utf-8") as fd:
-            current_sentence_tags = []  # list of (word, NER tag) tuple
+            current_line_tags = []  # list of (word, NER tag) tuple
             char_pos = 0
             for line in fd:
                 line = line.strip()
+
                 if line == "":
-                    # empty line represents sentence change
-                    if len(current_sentence_tags) > 0:
-                        self.parse_sentence(current_sentence_tags=current_sentence_tags, char_pos=char_pos)
+                    # empty line represents protocol step change
+                    if len(current_line_tags) > 0:
+                        self.parse_protocol_step(current_line_tags=current_line_tags, char_pos=char_pos)
 
-                        # update char_pos to end of latest added sentence
-                        char_pos = self.sentences[-1].end_char_pos
+                        # update char_pos to end of latest added protocol step
+                        char_pos = self.lines[-1].end_char_pos
 
-                    # now empty current_sentence_tags for the next sentence
-                    current_sentence_tags = []
-                    continue
+                    # now empty the current_line_tags for the next protocol step
+                    current_line_tags = []
+                else:
+                    tokens = line.split("\t")
+                    current_line_tags.append(tuple(tokens))
 
-                tokens = line.split("\t")
-                current_sentence_tags.append(tuple(tokens))
+            if len(current_line_tags) > 0:
+                self.parse_protocol_step(current_line_tags=current_line_tags, char_pos=char_pos)
 
-            if len(current_sentence_tags) > 0:
-                self.parse_sentence(current_sentence_tags=current_sentence_tags, char_pos=char_pos)
-
-    def parse_sentence(self, current_sentence_tags, char_pos):
-        """Parse sentence to populate words, sentences and entity annotations.
+    def parse_protocol_step(self, current_line_tags, char_pos):
+        """Parse protocol step to populate words, lines and entity annotations.
 
             Parameters:
             ----------
-            current_sentence_tags : list of tuple (word, NER tag)
+            current_line_tags : list of tuple (word, NER tag)
             char_pos : int (char position offset)
                         Current sentence text is searched from this offset onwards.
                         Usually its char position of end of the previous sentence.
+
+            See Also:
+            --------
+            parse_conll_annotation
         """
-        start_char_pos_sent = None
+        start_char_pos_line = None
         start_word_index = len(self.words)
         start_entity_ann_index = len(self.entity_annotations)
 
-        for i, (word, ner_tag) in enumerate(current_sentence_tags):
+        for i, (word, ner_tag) in enumerate(current_line_tags):
             start_char_pos_word = char_pos + self.text[char_pos:].find(word)
             end_char_pos_word = start_char_pos_word + len(word)
 
             if i == 0:
-                start_char_pos_sent = start_char_pos_word
+                start_char_pos_line = start_char_pos_word
 
             if ner_tag != "O":
                 split_index = ner_tag.find("-")
@@ -94,35 +167,109 @@ class Document:
             # update char position to the end of current word
             char_pos = end_char_pos_word
 
-        # append sentence to sentence list
-        self.sentences.append(Sentence(start_char_pos=start_char_pos_sent, end_char_pos=char_pos,
-                                       start_word_index=start_word_index, end_word_index=len(self.words)))
+        # store the count of sentences till now
+        n_sents_upto_prev_line = len(self.sentences)
+
+        # Now populate sentences and tokens
+        end_char_pos_line = char_pos
+        # reset char position to start position of protocol step
+        char_pos = start_char_pos_line
+
+        # Segment protocol step into sentence(s)
+        sents = self.nlp_process_obj.sent_tokenize(text=self.text[start_char_pos_line: end_char_pos_line])
+
+        for sent in sents:
+            # sent type: Span (https://spacy.io/api/span)
+            sent_text = sent.text
+            start_char_pos_relative = self.text[char_pos:].find(sent_text)
+            assert start_char_pos_relative >= 0,\
+                "Sentence not found from char position: {} onwards. Sentence text: {}".format(char_pos, sent_text)
+            start_char_pos_sent = char_pos + start_char_pos_relative
+            end_char_pos_sent = start_char_pos_sent + len(sent_text)
+
+            doc_sentence = self.nlp_process_obj.construct_doc(text=sent_text)
+            n_tokens_upto_prev_sent = len(self.tokens)
+
+            for token in doc_sentence:
+                start_char_pos_token = char_pos + self.text[char_pos:].find(token.text)
+                end_char_pos_token = start_char_pos_token + len(token.text)
+
+                head_index_token = token.head.i + n_tokens_upto_prev_sent
+                children_index_arr_token = [(child.i + n_tokens_upto_prev_sent) for child in token.children]
+
+                cur_token = Token(start_char_pos=start_char_pos_token, end_char_pos=end_char_pos_token,
+                                  part_of_speech=token.pos_, dependency_tag=token.dep_, head_index=head_index_token,
+                                  children_index_arr=children_index_arr_token)
+                self.tokens.append(cur_token)
+
+                # update char position to end of current token
+                char_pos = start_char_pos_token
+
+            # append sentence to sentence list
+            self.sentences.append(Sentence(start_char_pos=start_char_pos_sent, end_char_pos=end_char_pos_sent,
+                                           start_token_index=n_tokens_upto_prev_sent, end_token_index=len(self.tokens)))
+
+            # update char position to the end of current sentence
+            char_pos = end_char_pos_sent
+
+        # append line to line list
+        self.lines.append(Line(start_char_pos=start_char_pos_line, end_char_pos=char_pos,
+                               start_word_index=start_word_index, end_word_index=len(self.words),
+                               start_sent_index=n_sents_upto_prev_line, end_sent_index=len(self.sentences)))
 
     def display_document(self):
         ann_i = 0
-        for sent_i in range(len(self.sentences)):
-            start_char_pos_sent = self.sentences[sent_i].start_char_pos
-            end_char_pos_sent = self.sentences[sent_i].end_char_pos
-            sentence_text = self.text[start_char_pos_sent: end_char_pos_sent]
+        for line_i in range(len(self.lines)):
+            start_char_pos_line = self.lines[line_i].start_char_pos
+            end_char_pos_line = self.lines[line_i].end_char_pos
+            start_sent_index_line = self.lines[line_i].start_sent_index
+            end_sent_index_line = self.lines[line_i].end_sent_index
 
-            print("\n\nSentence #{} :: char pos range: ({}, {}) :: text: {}".format(
-                sent_i, start_char_pos_sent, end_char_pos_sent, sentence_text))
+            line_text = self.text[start_char_pos_line: end_char_pos_line]
 
-            start_word_index_sent = self.sentences[sent_i].start_word_index
-            end_word_index_sent = self.sentences[sent_i].end_word_index
+            print("\n\nLine #{} :: char pos range: ({}, {}) :: sent range: ({}, {}) :: text: {}".format(
+                line_i, start_char_pos_line, end_char_pos_line, start_sent_index_line, end_sent_index_line, line_text))
+
+            start_word_index_sent = self.lines[line_i].start_word_index
+            end_word_index_sent = self.lines[line_i].end_word_index
 
             for word_index in range(start_word_index_sent, end_word_index_sent):
                 word_text = self.text[self.words[word_index].start_char_pos: self.words[word_index].end_char_pos]
                 print("\tword #{}: {}".format(word_index, word_text))
 
-            # Find the entity annotations belonging to the sentence
+            for sent_index in range(start_sent_index_line, end_sent_index_line):
+                start_char_pos_sent = self.sentences[sent_index].start_char_pos
+                end_char_pos_sent = self.sentences[sent_index].end_char_pos
+                sent_text = self.text[start_char_pos_sent: end_char_pos_sent]
+
+                print("\nSentence #{} :: char pos range: ({}, {}) :: text: {}".format(
+                    sent_index, start_char_pos_sent, end_char_pos_sent, sent_text))
+
+                start_token_index_sent = self.sentences[sent_index].start_token_index
+                end_token_index_sent = self.sentences[sent_index].end_token_index
+
+                for token_index in range(start_token_index_sent, end_token_index_sent):
+                    start_char_pos_token = self.tokens[token_index].start_char_pos
+                    end_char_pos_token = self.tokens[token_index].end_char_pos
+                    token_text = self.text[start_char_pos_token: end_char_pos_token]
+                    token_part_of_speech = self.tokens[token_index].part_of_speech
+                    token_dependency_tag = self.tokens[token_index].dependency_tag
+                    head_index_token = self.tokens[token_index].head_index
+
+                    print("\tToken #{} :: char pos range: ({}, {}) :: text: {} :: POS: {} :: Dependency: {} :: "
+                          "Head:  {}".format(
+                        token_index, start_char_pos_token, end_char_pos_token, token_text, token_part_of_speech,
+                        token_dependency_tag, head_index_token))
+
+            # Find the entity annotations belonging to the line
             ann_index_sent_arr = []
 
             while ann_i < len(self.entity_annotations):
                 start_word_index_ann = self.entity_annotations[ann_i].start_word_index
                 end_word_index_ann = self.entity_annotations[ann_i].end_word_index
 
-                assert start_word_index_ann >= start_word_index_sent, "annotation assignment to sentence missed by previous sentence. ann_i: {}".format(ann_i)
+                assert start_word_index_ann >= start_word_index_sent,\
+                    "annotation assignment to line missed by previous line. ann_i: {}".format(ann_i)
 
                 if start_word_index_ann < end_word_index_sent:
                     ann_index_sent_arr.append(ann_i)
@@ -132,7 +279,7 @@ class Document:
                 ann_i += 1
 
             if len(ann_index_sent_arr) > 0:
-                print("\nEntity annotations belonging to the sentence:")
+                print("\nEntity annotations belonging to the step:")
                 for ann_index in ann_index_sent_arr:
                     start_word_index_ann = self.entity_annotations[ann_index].start_word_index
                     end_word_index_ann = self.entity_annotations[ann_index].end_word_index
@@ -158,25 +305,18 @@ class Document:
             print("\tEntity annotation #{} :: word index range: ({}, {}) :: type: {} :: text: {}".format(
                 ann_i, start_word_index_ann, end_word_index_ann, entity_type_ann, entity_text))
 
-class Sentence:
-    def __init__(self, start_char_pos=None, end_char_pos=None, start_word_index=None, end_word_index=None):
-        self.start_char_pos = start_char_pos
-        self.end_char_pos = end_char_pos
-        self.start_word_index = start_word_index
-        self.end_word_index = end_word_index
-
-
-class Word:
-    def __init__(self, start_char_pos=None, end_char_pos=None):
-        self.start_char_pos = start_char_pos
-        self.end_char_pos = end_char_pos
 
 if __name__ == "__main__":
-    protocol_id = 3
+    protocol_id = 101
     data_folder = "C:/KA/lib/WNUT_2020/data/train_data/"
     file_document = os.path.join(data_folder, "Standoff_Format/protocol_" + str(protocol_id) + ".txt")
     file_conll_ann = os.path.join(data_folder, "Conll_Format/protocol_" + str(protocol_id) + "_conll.txt")
-    document_obj = Document(doc_id=protocol_id)
+
+    obj_nlp_process = NLPProcess()
+    obj_nlp_process.load_nlp_model(verbose=True)
+    obj_nlp_process.build_sentencizer(verbose=True)
+
+    document_obj = Document(doc_id=protocol_id, nlp_process_obj=obj_nlp_process)
     document_obj.parse_document(document_file=file_document)
     document_obj.parse_conll_annotation(conll_ann_file=file_conll_ann)
     document_obj.display_document()
---
+++ src/nlp_process.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""
+References:
+----------
+https://spacy.io/api/sentencizer
+    Examples shows how to use Sentencizer.
+
+https://spacy.io/usage/linguistic-features#sbd
+    Default segmentation approach uses dependency parsing.
+    There's also option to create custom/rule based segmentation.
+
+    Referred by Ines in https://github.com/explosion/spaCy/issues/1756#issuecomment-387225769
+"""
+
+import spacy
+from spacy.lang.en import English
+from spacy.pipeline import Sentencizer
+
+
+class NLPProcess:
+    """NLP processing of text
+    """
+    def __init__(self, model="en_core_web_sm"):
+        self.model = model
+        self.nlp = None
+        self.nlp_sentencizer = None
+
+    def load_nlp_model(self, verbose=False):
+        self.nlp = spacy.load(name=self.model)
+        if verbose:
+            print("Model: {} loaded".format(self.model))
+            print("pipe names: {}".format(self.nlp.pipe_names))
+
+    def build_sentencizer(self, verbose=False):
+        self.nlp_sentencizer = English()
+        sentencizer = Sentencizer()
+        self.nlp_sentencizer.add_pipe(component=sentencizer)
+        if verbose:
+            print("pipe names: {}".format(self.nlp_sentencizer.pipe_names))
+
+    def sent_tokenize(self, text):
+        """Segment text into sentences."""
+        assert self.nlp_sentencizer is not None, "pre-requisite: Execute build_sentencizer()"
+        # TODO spaCy sentencizer fails in few cases e.g. protocol_101 (2nd line). Would need additional processing.
+        doc = self.nlp_sentencizer(text)
+        return doc.sents
+
+    def construct_doc(self, text):
+        """Construct Doc container from the text.
+
+            Reference:
+            ---------
+            https://spacy.io/api/doc
+        """
+        doc = self.nlp(text)
+        return doc
+
+
+if __name__ == "__main__":
+    nlp = English()
+    print(nlp.pipe_names)
+    sentencizer = Sentencizer()
+    nlp.add_pipe(sentencizer)
+    print(nlp.pipe_names)
+    doc = nlp("Login to the hpc (at the University of Arizona) with your user name Create a directory for the pipeline Go to the hpc-blast directory and create sub directories to hold std-err, std-out, data, blastdb, blast-results and scripts.")
+    print(list(doc.sents))
+    for token in doc:
+        print(token.text, token.pos_)
