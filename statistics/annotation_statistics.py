e82e839830b597152ca322fde74c3755d7d77f4f
Kaushik Acharya <acharya.kaushik@gmail.com>
2020-07-25 20:50:21 +0530

    Entity type wise dependency tag statistics

--- statistics/annotation_statistics.py
+++ statistics/annotation_statistics.py
@@ -12,13 +12,23 @@ import pandas as pd
 import re
 
 from ..src.dataset import *
+from ..src.nlp_process import *
 
 
 class AnnotationStatistics:
     def __init__(self):
         self.entity_statistics_dict = dict()
+        self.nlp_process_obj = None
+
+        self.build_nlp_process(verbose=True)
+
+    def build_nlp_process(self, verbose=True):
+        self.nlp_process_obj = NLPProcess()
+        self.nlp_process_obj.load_nlp_model(verbose=verbose)
+        self.nlp_process_obj.build_sentencizer(verbose=verbose)
 
     def process_dataset(self, data_dir):
+        """Process protocols in the dataset"""
         # Iterate over each of the files in the folder (train, dev)
         for f in os.listdir(os.path.join(data_dir, "Conll_Format")):
             print(f)
@@ -44,34 +54,37 @@ class AnnotationStatistics:
 
     def process_protocol(self, doc_id, doc_file, conll_ann_file):
         """Process a single protocol document"""
-        doc_obj = Document(doc_id=doc_id)
+        doc_obj = Document(doc_id=doc_id, nlp_process_obj=self.nlp_process_obj)
         doc_obj.parse_document(document_file=doc_file)
         doc_obj.parse_conll_annotation(conll_ann_file=conll_ann_file)
 
         ann_i = 0
-        for sent_i in range(len(doc_obj.sentences)):
-            start_word_index_sent = doc_obj.sentences[sent_i].start_word_index
-            end_word_index_sent = doc_obj.sentences[sent_i].end_word_index
+        # ??? Isn't that the text statistics can also be collected by iterating over the entities
 
-            # Find the entity annotations belonging to the sentence
-            ann_index_sent_arr = []
+        # iterate over the lines of the protocol where each line represents protocol step
+        for line_i in range(len(doc_obj.lines)):
+            start_word_index_line = doc_obj.lines[line_i].start_word_index
+            end_word_index_line = doc_obj.lines[line_i].end_word_index
+
+            # Find the entity annotations belonging to the protocol step
+            ann_index_line_arr = []
 
             while ann_i < len(doc_obj.entity_annotations):
                 start_word_index_ann = doc_obj.entity_annotations[ann_i].start_word_index
                 end_word_index_ann = doc_obj.entity_annotations[ann_i].end_word_index
 
-                assert start_word_index_ann >= start_word_index_sent,\
+                assert start_word_index_ann >= start_word_index_line,\
                     "annotation assignment to sentence missed by previous sentence. ann_i: {}".format(ann_i)
 
-                if start_word_index_ann < end_word_index_sent:
-                    ann_index_sent_arr.append(ann_i)
+                if start_word_index_ann < end_word_index_line:
+                    ann_index_line_arr.append(ann_i)
                 else:
                     break
 
                 ann_i += 1
 
             # Now populate statistics
-            for ann_index in ann_index_sent_arr:
+            for ann_index in ann_index_line_arr:
                 start_word_index_ann = doc_obj.entity_annotations[ann_index].start_word_index
                 end_word_index_ann = doc_obj.entity_annotations[ann_index].end_word_index
                 entity_type_ann = doc_obj.entity_annotations[ann_index].type
@@ -90,6 +103,55 @@ class AnnotationStatistics:
 
                 self.entity_statistics_dict[entity_type_ann].text_dict[entity_text_lower].append(doc_id)
 
+        token_i = 0
+        n_tokens = len(doc_obj.tokens)
+        for ann_index in range(len(doc_obj.entity_annotations)):
+            start_word_index_ann = doc_obj.entity_annotations[ann_index].start_word_index
+            end_word_index_ann = doc_obj.entity_annotations[ann_index].end_word_index
+            entity_type_ann = doc_obj.entity_annotations[ann_index].type
+
+            # --- extract the tokens in the current entity annotation ---
+            start_char_pos_ann = doc_obj.words[start_word_index_ann].start_char_pos
+            end_char_pos_ann = doc_obj.words[end_word_index_ann-1].end_char_pos
+            # N.B. end_word_index representing end of word index range is excluding word indexes belonging to the entity.
+            #       Hence char position end of the entity is chosen from the previous word index
+
+            # skip the tokens which are prior(in terms of char position) to the current entity annotation
+            while doc_obj.tokens[token_i].end_char_pos <= start_char_pos_ann:
+                token_i += 1
+                assert token_i < n_tokens,\
+                    "failed to identify the starting token matching entity annotation #{} :: " \
+                    "word index range: ({}, {}) :: char pos range: ({}, {})".format(
+                        ann_index, start_word_index_ann, end_word_index_ann, start_char_pos_ann, end_char_pos_ann)
+
+            start_token_index_ann = token_i
+
+            # identify the end of the token range matching the current entity annotation
+            # N.B. As per convention, end of the range excludes the annotation i.e. range: [)
+            token_i += 1
+            while (token_i < n_tokens) and (doc_obj.tokens[token_i].start_char_pos < end_char_pos_ann):
+                token_i += 1
+
+            end_token_index_ann = token_i
+
+            # Select the dependency tags for the heads which are outside of the token range of the entity annotation
+            dependency_tags = []
+            for token_index in range(start_token_index_ann, end_token_index_ann):
+                head_index = doc_obj.tokens[token_index].head_index
+                if (head_index == token_index) or (head_index < start_token_index_ann) or (head_index >= end_token_index_ann):
+                    dependency_tags.append(doc_obj.tokens[token_index].dependency_tag)
+
+            dependency_tags_str = "_".join(dependency_tags)
+
+            entity_type_ann in self.entity_statistics_dict,\
+            "entity type: {} should have been already in entity_statistics_dict. Entity annotation #{}".format(entity_type_ann, ann_index)
+
+            if dependency_tags_str not in self.entity_statistics_dict[entity_type_ann].dependency_dict:
+                self.entity_statistics_dict[entity_type_ann].dependency_dict[dependency_tags_str] = []
+
+            self.entity_statistics_dict[entity_type_ann].dependency_dict[dependency_tags_str].append(doc_id)
+
+
     def display_entity_tag_stats(self):
         for entity_tag in self.entity_statistics_dict:
             print("entity: {} :: count: {} :: unique text count: {}".format(
@@ -105,16 +167,31 @@ class AnnotationStatistics:
         for entity_tag in self.entity_statistics_dict:
             for entity_text_lower in self.entity_statistics_dict[entity_tag].text_dict:
                 doc_ids = self.entity_statistics_dict[entity_tag].text_dict[entity_text_lower]
-                csv_writer.writerow([entity_tag, entity_text_lower, doc_ids])
+                csv_writer.writerow([entity_tag, entity_text_lower, len(doc_ids), doc_ids])
 
         csv_output.seek(0)
 
-        df = pd.read_csv(filepath_or_buffer=csv_output, names=["entity", "text_lower", "doc_ids"])
+        df = pd.read_csv(filepath_or_buffer=csv_output, names=["entity", "text_lower", "count", "doc_ids"])
 
         output_dir = os.path.join(os.path.dirname(__file__), "../output/statistics")
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         output_file = os.path.join(output_dir, "entity_tags.csv")
+
+        df.to_csv(path_or_buf=output_file, index=False)
+
+        csv_dep_output = io.StringIO()
+        csv_dep_writer = writer(csv_dep_output)
+
+        for entity_tag in self.entity_statistics_dict:
+            for dependency_tags_str in self.entity_statistics_dict[entity_tag].dependency_dict:
+                doc_ids = self.entity_statistics_dict[entity_tag].dependency_dict[dependency_tags_str]
+                csv_dep_writer.writerow([entity_tag, dependency_tags_str, len(doc_ids), doc_ids])
+
+        csv_dep_output.seek(0)
+
+        df = pd.read_csv(filepath_or_buffer=csv_dep_output, names=["entity", "dependency", "count", "doc_ids"])
+        output_file = os.path.join(output_dir, "entity_dependency.csv")
 
         df.to_csv(path_or_buf=output_file, index=False)
 
@@ -125,6 +202,7 @@ class EntityStatistics:
         self.count = 0
         self.entity_tag = entity_tag
         self.text_dict = dict()  # key: text,  value: list of doc ids
+        self.dependency_dict = dict()
 
 
 if __name__ == "__main__":
