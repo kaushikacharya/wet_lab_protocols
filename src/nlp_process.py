0d7b8d5c8c4b6e4c2ebde9ba38ffd994cf7599a5
Kaushik Acharya <acharya.kaushik@gmail.com>
2020-07-28 10:48:54 +0530

    Dumping dependency parse trees

--- src/nlp_process.py
+++ src/nlp_process.py
@@ -4,7 +4,7 @@
 References:
 ----------
 https://spacy.io/api/sentencizer
-    Examples shows how to use Sentencizer.
+    Examples show how to use Sentencizer.
 
 https://spacy.io/usage/linguistic-features#sbd
     Default segmentation approach uses dependency parsing.
@@ -13,7 +13,10 @@ https://spacy.io/usage/linguistic-features#sbd
     Referred by Ines in https://github.com/explosion/spaCy/issues/1756#issuecomment-387225769
 """
 
+import argparse
+import os
 import spacy
+from spacy import displacy
 from spacy.lang.en import English
 from spacy.pipeline import Sentencizer
 
@@ -53,11 +56,49 @@ class NLPProcess:
             ---------
             https://spacy.io/api/doc
         """
+        assert self.nlp is not None, "pre-requisite: Execute load_nlp_model()"
         doc = self.nlp(text)
         return doc
 
+def main(args, verbose=True):
+    file_document = os.path.join(args.data_dir, "Standoff_Format/protocol_" + args.protocol_id + ".txt")
+
+    obj_nlp_process = NLPProcess()
+    obj_nlp_process.load_nlp_model(verbose=True)
+    obj_nlp_process.build_sentencizer(verbose=True)
+
+    output_dir = os.path.join(os.path.dirname(__file__), "../output/debug", args.protocol_id)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # iterate over the protocol steps i.e. lines of the protocol file
+    with open(file_document, encoding="utf-8") as fd_doc:
+        sent_i = 0
+        for line in fd_doc:
+            line = line.strip()
+            if line == "":
+                continue
+
+            sents = obj_nlp_process.sent_tokenize(text=line)
+
+            for sent in sents:
+                # sent type: Span (https://spacy.io/api/span)
+                sent_text = sent.text
+
+                doc_sentence = obj_nlp_process.construct_doc(text=sent_text)
+
+                if verbose:
+                    svg = displacy.render(doc_sentence, style="dep")
+
+                    svg_sentence_file = os.path.join(output_dir, str(sent_i) + ".svg")
+                    with open(svg_sentence_file, mode="w", encoding="utf-8") as fd:
+                        fd.write(svg)
+
+                # increment sentence index
+                sent_i += 1
 
 if __name__ == "__main__":
+    """
     nlp = English()
     print(nlp.pipe_names)
     sentencizer = Sentencizer()
@@ -67,3 +108,13 @@ if __name__ == "__main__":
     print(list(doc.sents))
     for token in doc:
         print(token.text, token.pos_)
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--protocol_id", action="store", dest="protocol_id")
+    parser.add_argument("--data_dir", action="store", default="C:/KA/lib/WNUT_2020/data/train_data/", dest="data_dir")
+
+    args = parser.parse_args()
+
+    main(args=args)
+
