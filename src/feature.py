c118615950eb0401558176439cb8cd5bdb4438c2
Kaushik Acharya <acharya.kaushik@gmail.com>
2020-08-16 23:50:41 +0530

    Extracting features of document

--- src/feature.py
+++ src/feature.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
-import os
+"""
+Feature extraction for a Document
+
+Reference:
+---------
+https://sklearn-crfsuite.readthedocs.io/en/latest/tutorial.html
+"""
+
+import argparse
 import spacy
 from spacy import displacy
 
@@ -8,54 +16,134 @@ from .dataset import *
 
 
 class Feature:
-    def __init__(self):
-        self.nlp = None
-
-    def load_nlp_model(self, model="en_core_web_sm", verbose=False):
-        self.nlp = spacy.load(name=model)
-        if verbose:
-            print("Model: {} loaded".format(model))
-
-    def extract_document_features(self, doc_obj, verbose=False):
-        """Extract features for the document.
-
+    """Extraction of features of a Document class(dataset.py).
+    """
+    def __init__(self, doc_obj):
+        """
             Parameters:
             ----------
             doc_obj : Document class object(dataset.py)
         """
-        assert self.nlp is not None, "pre-requisite: spacy model should be loaded"
+        self.doc_obj = doc_obj
 
-        output_dir = os.path.join(os.path.dirname(__file__), "../output/debug", str(doc_obj.id))
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+    def extract_document_features(self, verbose=False):
+        """Extract features for the document.
+        """
+        # document features: List of sentence features
+        # sentence feature: List of word features stored in dict
+        doc_features = []
 
-        # iterate over each of the sentences
-        for sent_i in range(len(doc_obj.sentences)):
-            start_char_pos = doc_obj.sentences[sent_i].start_char_pos
-            end_char_pos = doc_obj.sentences[sent_i].end_char_pos
-            sentence_text = doc_obj.text[start_char_pos: end_char_pos]
-
-            doc_sentence = self.nlp(sentence_text)
-
+        # iterate over each of the sentence
+        for sent_index in range(len(self.doc_obj.sentences)):
             if verbose:
-                svg = displacy.render(doc_sentence, style="dep")
+                start_char_pos = self.doc_obj.sentences[sent_index].start_char_pos
+                end_char_pos = self.doc_obj.sentences[sent_index].end_char_pos
+                sent_text = self.doc_obj.text[start_char_pos: end_char_pos]
+                start_word_index = self.doc_obj.sentences[sent_index].start_word_index
+                end_word_index = self.doc_obj.sentences[sent_index].end_word_index
+                start_token_index = self.doc_obj.sentences[sent_index].start_token_index
+                end_token_index = self.doc_obj.sentences[sent_index].end_token_index
 
-                svg_sentence_file = os.path.join(output_dir, str(sent_i)+".svg")
-                with open(svg_sentence_file, mode="w", encoding="utf-8") as fd:
-                    fd.write(svg)
+                print("\nSentence #{}: char pos range: ({}, {}) :: token index range: ({}, {}) :: "
+                      "word index range: ({}, {}) :: text: {}".format(
+                    sent_index, start_char_pos, end_char_pos, start_token_index, end_token_index, start_word_index,
+                    end_word_index, sent_text))
 
-if __name__ == "__main__":
-    feature_obj = Feature()
-    feature_obj.load_nlp_model(verbose=True)
+            sent_features = self.extract_sentence_features(sent_index=sent_index)
+            doc_features.append(sent_features)
 
-    protocol_id = 3
-    data_folder = "C:/KA/lib/WNUT_2020/data/train_data/"
-    file_document = os.path.join(data_folder, "Standoff_Format/protocol_" + str(protocol_id) + ".txt")
-    file_conll_ann = os.path.join(data_folder, "Conll_Format/protocol_" + str(protocol_id) + "_conll.txt")
+        return doc_features
 
-    document_obj = Document(doc_id=protocol_id)
+    def extract_sentence_features(self, sent_index):
+        """Extract sentence features.
+
+            Parameters:
+            ----------
+            sent_index : int
+
+            Returns
+            -------
+            List of word features
+        """
+        sent_features = []
+
+        start_word_index = self.doc_obj.sentences[sent_index].start_word_index
+        end_word_index = self.doc_obj.sentences[sent_index].end_word_index
+
+        for word_index in range(start_word_index, end_word_index):
+            word_obj = self.doc_obj.words[word_index]
+
+            word_features = dict()
+            word_features["bias"] = 1.0
+            word_text = self.doc_obj.text[word_obj.start_char_pos: word_obj.end_char_pos]
+            word_features["word_lower"] = word_text.lower()
+
+            if word_index == start_word_index:
+                word_features["BOS"] = True
+            else:
+                word_features["prev_word_lower"] = sent_features[-1]["word_lower"]
+
+            if word_obj.end_token_index - word_obj.start_token_index > 1:
+                word_features["multi_token"] = True
+
+            # TODO In case of multi-tokens, select syntactic features from the prime token
+            word_features["POS"] = self.doc_obj.tokens[word_obj.start_token_index].part_of_speech
+            word_features["dependency"] = self.doc_obj.tokens[word_obj.start_token_index].dependency_tag
+
+            # append word features to the list of sentence features
+            sent_features.append(word_features)
+
+        return sent_features
+
+def main(args):
+    import os
+    from .nlp_process import NLPProcess
+
+    file_document = os.path.join(args.data_dir, "Standoff_Format/protocol_" + args.protocol_id + ".txt")
+    file_conll_ann = os.path.join(args.data_dir, "Conll_Format/protocol_" + args.protocol_id + "_conll.txt")
+
+    obj_nlp_process = NLPProcess()
+    obj_nlp_process.load_nlp_model(verbose=True)
+    obj_nlp_process.build_sentencizer(verbose=True)
+
+    document_obj = Document(doc_id=int(args.protocol_id), nlp_process_obj=obj_nlp_process)
     document_obj.parse_document(document_file=file_document)
     document_obj.parse_conll_annotation(conll_ann_file=file_conll_ann)
 
-    feature_obj.extract_document_features(doc_obj=document_obj, verbose=True)
+    feature_obj = Feature(doc_obj=document_obj)
+    doc_features = feature_obj.extract_document_features(verbose=True)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--protocol_id", action="store", dest="protocol_id")
+    parser.add_argument("--data_dir", action="store", default="C:/KA/lib/WNUT_2020/data/train_data/", dest="data_dir")
+
+    args = parser.parse_args()
+    main(args=args)
+
+"""
+Tasks:
+    - Features hierarchy:
+        - Document -> Line/Sentence -> Word/Token
+
+    - Feature groups:
+        - POS
+        - Lexical
+        - Dependency
+
+    - Lexical features:
+        - word especially which are mostly unique property for a entity class e.g. temperature
+        - Begin of sentence
+            - Also if conjunction with begin of sentence
+        - Word mapped to multi-token
+            - Handle their POS, dependency features
+        - Numerals
+            - 200 µL
+            - 80%
+
+    - Dependency features:
+        - dependency type
+        - dependent words
+        - governor words
+        - dependency consistency features as mentioned in Jie et al  papers/NLP/NER
+"""
