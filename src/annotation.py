5f83dc79bfc309f36286d320cb340524c8208fac
Kaushik Acharya <acharya.kaushik@gmail.com>
2020-08-24 23:22:35 +0530

    Added fields: start/end char position

--- src/annotation.py
+++ src/annotation.py
@@ -5,9 +5,11 @@ class EntityAnnotation:
     """By entity we define 'Action' as well as entities.
         This class is tied to a particular Document.
     """
-    def __init__(self, entity_id=None, start_word_index=None, end_word_index=None, entity_type=None):
+    def __init__(self, entity_id=None, start_char_pos=None, end_char_pos=None, start_word_index=None, end_word_index=None, entity_type=None):
         # id: defined in .ann file. Its defined as 'T' followed by numeral.
         self.id = entity_id
+        self.start_char_pos = start_char_pos
+        self.end_char_pos = end_char_pos
         # word index range: [start_word_index, end_word_index)
         self.start_word_index = start_word_index
         self.end_word_index = end_word_index
